90cd85fd64183dab2bf1ee507355d2abaa0013ee
Lu <lsadfbnm@gmail.com>
2018-05-17 11:16:48 +0800

    main

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int IPhone,Sum;
+int IPhone,Sum;/*變數宣告*/ 
 int main(int argc, char *argv[]) {
-	for(IPhone=0;IPhone<20;IPhone++){
-		Sum=Sum+IPhone;
+	for(IPhone=0;IPhone<20;IPhone++)/*從1加到20*/
+	{
+		Sum=Sum+IPhone;/*和*/ 
 	}
-	printf("Sum=%d",Sum);
+	printf("Sum=%d",Sum);/*印出和*/ 
 		return 0;
 }
